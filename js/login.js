5ef5dcfd4583ed675d8a3a96a663bf64656fffa8
Anson Ng <914anson5@gmail.com>
2022-11-29 23:50:03 +0800

    feat: complete client-side textbox input validation for login credentials
    add: unified invalid input label style

--- js/login.js
+++ js/login.js
@@ -1,15 +1,62 @@
 const signInToggle = document.querySelector("#signin-toggle"),
-    pwdConfirmationRow = document.querySelector("#pwd-confirmation-row"),
-    credentialSubmitButton = document.querySelector("#credential-submit-btn");
+    credentialSubmitButton = document.querySelector("#credential-submit-btn"),
+    invalidEmailLabel = document.querySelector("#invalid-email-label")
 
+// UI
 credentialSubmitButton.innerHTML=`Log in`;
 
 signInToggle.addEventListener("change", () => {
     if (signInToggle.checked){
-        pwdConfirmationRow.style.display="block";
         credentialSubmitButton.innerHTML=`Sign up`;
     } else {
-        pwdConfirmationRow.style.display="none";
         credentialSubmitButton.innerHTML=`Log in`;
+    }
+})
+
+// Input Validation
+const emailInput=document.querySelector("#login-email"),
+    passwordInput=document.querySelector("#login-password");
+
+function validateEmail(email){
+    const re = /^\S+@\S+\.\S/;
+    return re.test(email);
+}
+
+function validatePassword(password){
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-z\d@$!%*#?&]{8,32}$/;
+    return re.test(password);
+}
+
+function invalidEmailAlert(){
+    if (!validateEmail(emailInput.value) && emailInput.value !== "") {
+        invalidEmailLabel.style.display = "block";
+        emailInput.style.color = "#ce034c"
+    } else {
+        invalidEmailLabel.style.display = "none";
+        emailInput.style.color = "black"
+    }
+}
+
+function invalidPasswordAlert() {
+    if (!validatePassword(passwordInput.value) && passwordInput.value !== "") {
+        passwordInput.style.color = "#ce034c";
+    } else {
+        passwordInput.style.color = "";
+    }
+}
+
+signInToggle.addEventListener("change", () => {
+    if (signInToggle.checked){
+        invalidEmailAlert();
+        invalidPasswordAlert();
+        emailInput.addEventListener("input", () => {
+            invalidEmailAlert();
+        })
+        passwordInput.addEventListener("input", () => {
+            invalidPasswordAlert();
+        })
+    } else {
+        invalidEmailLabel.style.display = "none";
+        passwordInput.style.color = "black";
     }
 })
