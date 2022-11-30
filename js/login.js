a6f47baa9514eded7c7b690c12574699519c7ade
Anson Ng <914anson5@gmail.com>
2022-11-30 11:43:10 +0800

    feat #10: add password requirement indicator box and disable button if credentials are invalid
    add: unified invalid input label style

--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 const signInToggle = document.querySelector("#signin-toggle"),
-    credentialSubmitButton = document.querySelector("#credential-submit-btn"),
-    invalidEmailLabel = document.querySelector("#invalid-email-label")
+    credentialSubmitButton = document.querySelector("#credential-submit-btn");
 
 // UI
 credentialSubmitButton.innerHTML=`Log in`;
@@ -14,8 +13,14 @@ signInToggle.addEventListener("change", () => {
 })
 
 // Input Validation
-const emailInput=document.querySelector("#login-email"),
-    passwordInput=document.querySelector("#login-password");
+const emailTextbox=document.querySelector("#login-email-box"),
+    passwordTextbox=document.querySelector("#login-password-box"),
+    invalidEmailLabel = document.querySelector("#invalid-email-label"),
+    passwordRequirementContainer = document.querySelector(".password-requirement-container"),
+    lengthIndicator = document.querySelector("#length-indicator"),
+    characterIndicator = document.querySelector("#character-indicator"),
+    numberIndicator = document.querySelector("#number-indicator"),
+    symbolIndicator = document.querySelector("#symbol-indicator");
 
 function validateEmail(email){
     const re = /^\S+@\S+\.\S/;
@@ -23,40 +28,95 @@ function validateEmail(email){
 }
 
 function validatePassword(password){
-    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-z\d@$!%*#?&]{8,32}$/;
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&.])[a-zA-z\d@$!%*#?&.]{8,32}$/;
     return re.test(password);
 }
 
+function checkLength(password){
+    if (password.length >= 8 && password.length <= 32) {
+        lengthIndicator.style.borderColor = "#00b2a3";
+    } else {
+        lengthIndicator.style.borderColor = "#ff4e8e";
+    }
+}
+
+function checkCharacter(password){
+    const re = /^(?=.*[a-z])(?=.*[A-Z]).*$/;
+    if (re.test(password)) {
+        characterIndicator.style.borderColor = "#00b2a3";
+    } else {
+        characterIndicator.style.borderColor = "#ff4e8e";
+    }
+}
+
+function checkNumber(password){
+    const re = /^(?=.*\d).*$/;
+    if (re.test(password)) {
+        numberIndicator.style.borderColor = "#00b2a3";
+    } else {
+        numberIndicator.style.borderColor = "#ff4e8e";
+    }
+}
+
+function checkSymbol(password){
+    const re = /^(?=.*[@$!%*#?&.]).*$/;
+    if (re.test(password)) {
+        symbolIndicator.style.borderColor = "#00b2a3";
+    } else {
+        symbolIndicator.style.borderColor = "#ff4e8e";
+    }
+}
+
 function invalidEmailAlert(){
-    if (!validateEmail(emailInput.value) && emailInput.value !== "") {
+    if (!validateEmail(emailTextbox.value) && emailTextbox.value !== "") {
         invalidEmailLabel.style.display = "block";
-        emailInput.style.color = "#ce034c"
+        emailTextbox.style.color = "#ce034c";
     } else {
         invalidEmailLabel.style.display = "none";
-        emailInput.style.color = "black"
+        emailTextbox.style.color = "black";
     }
 }
 
 function invalidPasswordAlert() {
-    if (!validatePassword(passwordInput.value) && passwordInput.value !== "") {
-        passwordInput.style.color = "#ce034c";
+    if (!validatePassword(passwordTextbox.value) && passwordTextbox.value !== "") {
+        passwordTextbox.style.color = "#ce034c";
     } else {
-        passwordInput.style.color = "";
+        passwordTextbox.style.color = "black";
     }
 }
 
 signInToggle.addEventListener("change", () => {
     if (signInToggle.checked){
+
+        passwordRequirementContainer.style.display = "block";
+
         invalidEmailAlert();
         invalidPasswordAlert();
-        emailInput.addEventListener("input", () => {
+        checkLength(passwordTextbox.value);
+        checkCharacter(passwordTextbox.value);
+        checkNumber(passwordTextbox.value);
+        checkSymbol(passwordTextbox.value);
+        credentialSubmitButton.disabled = !(validateEmail(emailTextbox.value) && validatePassword(passwordTextbox.value));
+
+        emailTextbox.addEventListener("input", () => {
             invalidEmailAlert();
+            credentialSubmitButton.disabled = !(validateEmail(emailTextbox.value) && validatePassword(passwordTextbox.value));
         })
-        passwordInput.addEventListener("input", () => {
+
+        passwordTextbox.addEventListener("input", () => {
             invalidPasswordAlert();
+            checkLength(passwordTextbox.value);
+            checkCharacter(passwordTextbox.value);
+            checkNumber(passwordTextbox.value);
+            checkSymbol(passwordTextbox.value);
+            credentialSubmitButton.disabled = !(validateEmail(emailTextbox.value) && validatePassword(passwordTextbox.value));
         })
+
     } else {
+        passwordRequirementContainer.style.display = "none";
         invalidEmailLabel.style.display = "none";
-        passwordInput.style.color = "black";
+        emailTextbox.style.color = "black";
+        passwordTextbox.style.color = "black";
+        credentialSubmitButton.disabled = false;
     }
 })
